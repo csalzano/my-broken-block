4c0d592557f44e5c03b4937ac5aec884642e785e
Corey Salzano <corey.salzano@gmail.com>
2020-02-17 17:08:20 -0500

    Changes spaces to tabs in src/index.js

--- src/index.js
+++ src/index.js
@@ -5,40 +5,40 @@ registerBlockType( 'myguten/test-block', { //Block names must include only lower
 	title: 'My Broken Block', //what the user sees
 	icon: 'universal-access-alt', //dashicon
 	category: 'layout', //block category
-    attributes: {
-        blockValue: {
-            type: 'string',
-            source: 'meta',
-            meta: 'block_meta_key', //the meta key where the value gets saved
-        },
-    },
+	attributes: {
+		blockValue: {
+			type: 'string',
+			source: 'meta',
+			meta: 'block_meta_key', //the meta key where the value gets saved
+		},
+	},
 
-    example: {
-        attributes: {
-            blockValue: 'Some value',
-        },
-    },
+	example: {
+		attributes: {
+			blockValue: 'Some value',
+		},
+	},
 
-    edit( { className, setAttributes, attributes } ) {
-        // function updateBlockValue( newValue ) {
-        //     setAttributes( { blockValue: newValue } );
-        // }
+	edit( { className, setAttributes, attributes } ) {
+		// function updateBlockValue( newValue ) {
+		//     setAttributes( { blockValue: newValue } );
+		// }
 
-        return (
-            <div className={ className }>
-                <TextControl
-                    label="My Broken Block"
-                    value={ attributes.blockValue }
-                    //onChange={ (newValue) => updateBlockValue( newValue ) }
-                    onChange={(newtext) => setAttributes({ blockValue: newtext })}
-                />
-            </div>
-        );
-    },
+		return (
+			<div className={ className }>
+				<TextControl
+					label="My Broken Block"
+					value={ attributes.blockValue }
+					//onChange={ (newValue) => updateBlockValue( newValue ) }
+					onChange={(newtext) => setAttributes({ blockValue: newtext })}
+				/>
+			</div>
+		);
+	},
 
-    // No information saved to the block
-    // Data is saved to post meta via attributes
-    save() {
-        return null;
-    },
+	// No information saved to the block
+	// Data is saved to post meta via attributes
+	save() {
+		return null;
+	},
 } );
